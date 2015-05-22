1233328b8d640d1a3987376fe05b7f2266973d5b
momchilDishev <mo.dishev@gmail.com>
2015-05-22 14:42:20 +0300

    Username to Register and Login added, Email-login removed, views edited - all working

--- Freelancer.MVC/Models/AccountViewModels.cs
+++ Freelancer.MVC/Models/AccountViewModels.cs
@@ -48,10 +48,10 @@ namespace Freelancer.MVC.Models
 
     public class LoginViewModel
     {
+
         [Required]
-        [Display(Name = "Email")]
-        [EmailAddress]
-        public string Email { get; set; }
+        [Display(Name = "Username")]
+        public string Username { get; set; }
 
         [Required]
         [DataType(DataType.Password)]
@@ -64,6 +64,10 @@ namespace Freelancer.MVC.Models
 
     public class RegisterViewModel
     {
+        [Required]
+        [Display(Name = "Username")]
+        public string Username { get; set; }
+
         [Required]
         [EmailAddress]
         [Display(Name = "Email")]
